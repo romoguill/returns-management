78201c2b8f51210b9d0537138af75bdf33356970
Rodrigo Moguillanksy <romoguill@gmail.com>
2021-12-15 06:27:16 -0300

    Finished the filters of the movements for client productID and flow. Missing dates

--- public/scripts/movements-management.js
+++ public/scripts/movements-management.js
@@ -7,8 +7,49 @@ const filterStatusElement = document.getElementById('status');
 
 const movementsElements = document.querySelectorAll('tbody tr');
 
-console.log(movementsElements);
-
 const movementsArray = Array.from(movementsElements);
 
-console.log(movementsArray);
+const filters = {};
+
+function applyFilters() {
+  for (const row of movementsArray) {
+    if (
+      (filters.productId &&
+        row.getElementsByTagName('td')[1].textContent.trim() !==
+          filters.productId) ||
+      (filters.client &&
+        row.getElementsByTagName('td')[2].textContent.trim() !==
+          filters.client) ||
+      (filters.flow &&
+        row.getElementsByTagName('td')[3].textContent.trim() !==
+          filters.flow) ||
+      (filters.status &&
+        row.getElementsByTagName('td')[4].textContent.trim() !== filters.status)
+    ) {
+      row.style.display = 'none';
+    } else {
+      row.style.display = 'table-row';
+    }
+  }
+  console.log(movementsArray);
+}
+
+filterProductIdElement.addEventListener('input', (e) => {
+  filters.productId = e.target.value;
+  applyFilters();
+});
+
+filterFlowElement.addEventListener('input', (e) => {
+  filters.flow = e.target.value;
+  applyFilters();
+});
+
+filterClientElement.addEventListener('input', (e) => {
+  filters.client = e.target.value;
+  applyFilters();
+});
+
+filterStatusElement.addEventListener('input', (e) => {
+  filters.status = e.target.value;
+  applyFilters();
+});
